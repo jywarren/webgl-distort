9ec6a60101b25bad4c05eea36eaee52eaf07fc44
jywarren <jeff@unterbahn.com>
2016-03-17 00:49:31 -0400

    blob-based download instead of data-url

--- dist/webgl-distort.js
+++ dist/webgl-distort.js
@@ -1,3 +1,26 @@
+// http://stackoverflow.com/questions/4998908/convert-data-uri-to-file-then-append-to-formdata
+
+function dataURItoBlob(dataURI) {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+        byteString = atob(dataURI.split(',')[1]);
+    else
+        byteString = unescape(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], {type:mimeString});
+}
+
+
 warpWebGl = function(id, matrix1, matrix2) {
 
   // try to create a WebGL canvas (will fail if WebGL isn't supported)
@@ -15,10 +38,15 @@ warpWebGl = function(id, matrix1, matrix2) {
   canvas.draw(texture).perspective(matrix1, matrix2).update();
 
   // replace the image with the canvas
-  image.parentNode.insertBefore(canvas, image);
-  image.parentNode.removeChild(image);
+  // image.parentNode.insertBefore(canvas, image);
+  // image.parentNode.removeChild(image);
 
   // replace the image
   //image.src = canvas.toDataURL('image/png');
+  //window.location = canvas.toDataURL('image/png');
+
+  var blob = dataURItoBlob(canvas.toDataURL('image/png'));
+  var burl = window.URL.createObjectURL(blob);
+  window.open(burl);
 
 }
